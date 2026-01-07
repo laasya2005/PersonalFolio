a6343e199d319e08c70f09fabcafdf779a328537
indirameka <indira.meka@gmail.com>
2026-01-06 23:03:25 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/my-personal-journey/",
+  
   server: {
     host: "::",
     port: 8080,
